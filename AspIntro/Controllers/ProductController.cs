60490b0e9ad4cddfa4177efe1a721f3c82bba3a8
Murad Mammadzade <mammadzade03@gmail.com>
2023-07-05 00:07:20 +0400

    done

--- AspIntro/Controllers/ProductController.cs
+++ AspIntro/Controllers/ProductController.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspIntro.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspIntro.Controllers
 {
     public class ProductController : Controller
     {
+        private List<Product> _products;
+        public ProductController()
+        {
+            _products = new()
+            {
+                new(){Id=1,Name="Telefon"},
+                new(){Id=2,Name="Qabyuyan"},
+                new(){Id=3,Name="Paltaryuyan"},
+            };
+        }
         public IActionResult Index()
         {
+            ViewBag.Products= _products;
             return View();
         }
-        public IActionResult Detail()
+        public IActionResult Detail(int id)
         {
+            var item=_products.Find(x => x.Id == id);
+            ViewBag.Data= item;
             return View();
         }
     }
--- AspIntro/Program.cs
+++ AspIntro/Program.cs
@@ -7,5 +7,15 @@ app.MapControllerRoute(
     "default",
     "{controller=home}/{action=index}/{id?}"
     );
+app.MapControllerRoute(
+    "about",
+    pattern:"about-us",
+    defaults:new {controller="home",action="about"}
+    );
+app.MapControllerRoute(
+    "contact",
+    pattern: "contact-us",
+    defaults: new { controller = "home", action = "contact" }
+    );
 
 app.Run();
